278299afe518fab87e399e936b01f931132e9779
Tilek5 <tilek.talantbekov@alatoo.edu.kg>
2024-09-26 10:00:40 +0600

    Update Calculator .py

--- Calculator .py
+++ Calculator .py
@@ -1,21 +1,25 @@
 from os import system
 
 a = round(int(input('Write the first number: ')))
-s = round(int(input('Write the second number: ')))
-operators = input('Write the operations u want(+ - * /:) ')
-if(operators == '+'):
+b = round(int(input('Write the second number: ')))
+operators = input('Choose operations: \n'
+                  "1 - Multiplication \n"
+                  "2 - Divide \n"
+                  "3 - Substraction \n"
+                  "4 - Addition \n")
+if(operators == '4'):
     _ = system('cls')
-    asd = a + s
-    print(f'{a} + {s} = {asd}')
-elif(operators == '-'):
+nswer = x + y
+    print(f'{x} + {y} = answer}')
+elif(operators == '3'):
     _ = system('cls')
-    asd = a - s
-    print(f'{a} - {s} = {asd}')
-elif(operators == '*'):
+nswer = x - y
+    print(f'{x} - {y} = nswer}')
+elif(operators == '1'):
     _ = system('cls')
-    asd = a * s
-    print(f'{a} * {s} = {asd}')
+nswer = x * y
+    print(f'{x} * {y} = nswer}')
 else:
     _ = system('cls')
-    asd = a / s
-    print(f'{a} / {s} = {asd}')
+nswer = x / y
+    print(f'{x} / {y} = nswer}')
